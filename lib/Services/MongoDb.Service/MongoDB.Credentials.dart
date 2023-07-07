3f37acaf35c70abee709da647912f7ea1aef00bf
Huloiarnata <rk710.nr@gmail.com>
2023-07-07 23:48:18 +0530

    Updates

--- lib/Services/MongoDb.Service/MongoDB.Credentials.dart
+++
@@ -1,5 +0,0 @@
-//EAogib6OZ4ZzJkg0
-// ignore_for_file: constant_identifier_names
-
-const MONGODB_CONN_URL = 'mongodb+srv://ronitofficial710:EAogib6OZ4ZzJkg0@sunshine.gakwjrq.mongodb.net/Sunshine?retryWrites=true&w=majority';
-const USER_COLLECTION = "GoogleUserData";
